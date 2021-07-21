e86ad65cb21f160f2b660a54448ac79d6afba352
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-21 16:02:33 -0300

    Segunda Aula
    42min

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import ilustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 import "../styles/auth.scss";
+import { Button } from "../components/Button";
 
 export function Home() {
   return (
@@ -24,7 +25,7 @@ export function Home() {
           <div className="separator">Ou entre em uma sala</div>
           <form action="">
             <input type="text" placeholder="Digite o código da sala" />
-            <button type="submit">Entrar na sala</button>
+            <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
       </main>
