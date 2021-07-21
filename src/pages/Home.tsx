a43fb6d8b2672eaed1f3a0ef48787c9c951df03f
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-21 11:55:18 -0300

    Segunda Aula
    26min

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import ilustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
+import '../styles/auth.scss'
+
 
 export function Home(){
   return (
-    <div>
+    <div id="pages-auth"> 
       <aside>
         <img src={ilustrationImg} alt="ilustração simbolizando perguntas e respostas" />
         <strong>Crie sala de Q&amp;A ao-vivo</strong>
