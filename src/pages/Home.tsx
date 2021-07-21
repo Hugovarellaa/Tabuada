a98c4f428269bf0eff6ac6e3102fa2fc8203a618
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-21 14:46:37 -0300

    Segunda Aula
    34min

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,33 @@
-import ilustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
-import '../styles/auth.scss'
+import ilustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
+import "../styles/auth.scss";
 
-
-export function Home(){
+export function Home() {
   return (
-    <div id="pages-auth"> 
+    <div id="pages-auth">
       <aside>
-        <img src={ilustrationImg} alt="ilustração simbolizando perguntas e respostas" />
+        <img
+          src={ilustrationImg}
+          alt="ilustração simbolizando perguntas e respostas"
+        />
         <strong>Crie sala de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiencia em tempo-real</p>
       </aside>
-      <main >
+      <main>
         <div className="main-content">
           <img src={logoImg} alt="logo letmeask" />
           <button>
             <img src={googleIconImg} alt="logo do google" />
             Crie sua sala com o Google
           </button>
-          <div>Ou entre em uma sala</div>
+          <div className="separator">Ou entre em uma sala</div>
           <form action="">
-            <input 
-            type="text" 
-            placeholder="Digite o código da sala"
-            />
-            <button type="submit" >
-              Entrar na sala
-            </button>
+            <input type="text" placeholder="Digite o código da sala" />
+            <button type="submit">Entrar na sala</button>
           </form>
         </div>
-
       </main>
     </div>
-  )
+  );
 }
